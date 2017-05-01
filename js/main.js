d5f6ee82d55ddb46126f403ac6427c3e5181afaf
pritam-das <pritam.das@mail.utoronto.ca>
2017-05-01 16:36:45 -0400

    Assignment complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
+$(function(){
+
+$('.plate').on('click', function(){
+
+  if ($('body').attr('class') === 'light')
+  {
+    $('.status').text("Who turned off the lights?");
+    $('.switch').attr('class', 'switch off');
+    $('body').attr('class', 'dark');
+  }
+
+  else
+  {
+      $('.status').text("It's so bright in here!");
+      $('.switch').attr('class', 'switch off');
+      $('body').attr('class', 'light');
+
+  }
 
 
+
+
+
+});
+});
